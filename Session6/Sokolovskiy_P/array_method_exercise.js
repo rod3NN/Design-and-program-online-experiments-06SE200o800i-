ba48cdf4ec05aabc7df48e8b442052c0a2d26aad
rod3NN <137772841+rod3NN@users.noreply.github.com>
2026-03-24 11:45:35 +0100

    in-class practice s6

--- Session6/Sokolovskiy_P/array_method_exercise.js
+++ Session6/Sokolovskiy_P/array_method_exercise.js
@@ -30,6 +30,11 @@ const trials = [
 
 let correctCount = 0;
 // start add your code here
+trials.forEach((trial) => {
+  if (trial.correct) {
+    correctCount++;
+  }
+});
 
 // your code end
 console.log("Correct answers:", correctCount);
